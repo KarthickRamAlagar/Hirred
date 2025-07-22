54667c391a1ae492787cc576ab40dde6e7bfd2d5
karthickramalagare <karthickramalagr@gmail.com>
2025-07-23 00:50:14 +0530

    fix:Case-Sensitive Issues

--- src/pages/JobListing.jsx
+++ src/pages/JobListing.jsx
@@ -4,7 +4,7 @@ import { State } from "country-state-city";
 import { BarLoader } from "react-spinners";
 
 import useFetch from "@/hooks/useFetch";
-import JobCard from "@/components/jobCard";
+import JobCard from "@/components/JobCard";
 import NoFoundCard from "@/components/NoFoundCard";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
